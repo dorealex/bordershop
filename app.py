b4dc64274ca5c421ffe86a85731f7ba503fdb142
dorealex <dorealex1@gmail.com>
2021-12-05 21:52:50 -0500

    fixed countries logic

--- app.py
+++ app.py
@@ -88,17 +88,18 @@ def writeRoute(route):
     timezone = route['timezone']    #5
     local_arrive = get_local_one(utc_arriv, timezone)
     cols = st.columns(5)
+    st.write('-----')
     cols[0].write('**Route**')
     cols[0].write(routeName)
     cols[1].write('**Crossing Name**')
     cols[1].write(name)
     cols[2].write('**Province**')
     cols[2].write(prov)
-    cols[3].write('**Est. Arrival Time (Local Time)**')
+    cols[3].write('**Est. Border Arrival Time (Local Time)**')
     cols[3].write(local_arrive)
     cols[4].write('**Est. Wait**')
     cols[4].write(formatWait(wait))
-    st.write('-----')
+    
 
 for n in names:
     items.append(" - "+n)
@@ -254,20 +255,38 @@ with st.expander('User: Traveller'):
     time = tcol[1].time_input('Time?', dt.datetime.now())
     submit = st.button('Estimate')
     dtime = dt.datetime.combine(when,time)
+    gc = gmaps.geocode(stop)
+    for x in gc[0]['address_components']:
+        if 'country' in x['types']:
+            ctry = x['short_name']
+            if ctry == 'CA':
+                stop_ok = True
+            else:
+                stop_ok = False
     if submit:
         if use:
-            loc = gmaps.geolocate()['location']        
+            pos = gmaps.geolocate()['location']        
+            #pos = {'lat': 48.512680, 'lng': -111.871895} # 48.512680, -111.871895
+            gc = gmaps.reverse_geocode(pos)
         else:
-            loc = gmaps.geocode(start)[0]['geometry']['location']
-        dep_tz = gmaps.timezone(loc)['timeZoneId']        
-        dtime = dtime.astimezone(pytz.timezone(dep_tz))        
-        st.components.v1.iframe(mongo_queries.mapmaker(start = loc, dest =stop,key=mongo_queries.api_key),height=400)
-        routeinfo = mongo_queries.get_trip_wait(loc,stop,dtime)
-        #routeinfo = test_dict
-        #df = pd.DataFrame(routeinfo)
+            #loc = gmaps.geocode(start)[0]['geometry']['location']
+            gc = gmaps.geocode(start)
+        for x in gc[0]['address_components']:
+            if 'country' in x['types']:
+                ctry = x['short_name']
+        if ctry == 'US' and stop_ok:    
+            loc = gc[0]['geometry']['location']
+            #loc = gmaps.geocode(start)[0]['geometry']['location']
         
-        for r in routeinfo:        
-            writeRoute(r)
+            dep_tz = gmaps.timezone(loc)['timeZoneId']        
+            dtime = dtime.astimezone(pytz.timezone(dep_tz))        
+            st.components.v1.iframe(mongo_queries.mapmaker(start = loc, dest =stop,key=mongo_queries.api_key),height=400)
+            routeinfo = mongo_queries.get_trip_wait(loc,stop,dtime)
+            #routeinfo = test_dict
+            #df = pd.DataFrame(routeinfo)
+            
+            for r in routeinfo:        
+                writeRoute(r)
 
 with st.expander("Metadata"):
     st.write("""

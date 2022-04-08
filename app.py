ef3dd149de494f60b6bf5c22296a68648885368f
dorealex <dorealex1@gmail.com>
2022-04-08 12:44:20 -0400

    changed to forms

--- app.py
+++ app.py
@@ -214,162 +214,172 @@ with st.expander('How this works'):
 #     st.markdown(md)
 
 with st.expander('User: Management'):
-    region_sel = st.selectbox('Region',['All']+mongo_queries.get_regions())
-    district_sel = st.selectbox('District',['All']+mongo_queries.get_district(region_sel))
-    port_choices = ['All']
-    if region_sel =='All' and district_sel == 'All':
-        port_choices =['Currently being updated','All']
-    port_sel = st.selectbox('Border Crossing',port_choices+mongo_queries.get_ports(region_sel,district_sel))
+    with st.form(key='manager'):
+
+        region_sel = st.selectbox('Region',['All']+mongo_queries.get_regions())
+        district_sel = st.selectbox('District',['All']+mongo_queries.get_district(region_sel))
+        port_choices = ['All']
+        if region_sel =='All' and district_sel == 'All':
+            port_choices =['Currently being updated','All']
+        port_sel = st.selectbox('Border Crossing',port_choices+mongo_queries.get_ports(region_sel,district_sel))
 
 
-    metric = st.selectbox("Metric", ['Average','Maximum','Median'], index=0, key=None, help=None, on_change=None, args=None, kwargs=None)
-    timeframe = st.selectbox("Timeframe", ['All Time','1 week','1 day', '1 month', '1 quarter', '1 year' ])
-    f={}
-    zoom=2
-    if region_sel !='All':
-        zoom=5
-        f.update({'region':region_sel})
-    if district_sel !='All':
-        zoom=8
-        f.update({'district':district_sel})
-    if port_sel !='All':
-        if port_sel =='Currently being updated':
-            f = {'id':{'$in':sites}}
-            zoom=3
+        metric = st.selectbox("Metric", ['Average','Maximum','Median'], index=0, key=None, help=None, on_change=None, args=None, kwargs=None)
+        timeframe = st.selectbox("Timeframe", ['All Time','1 week','1 day', '1 month', '1 quarter', '1 year' ])
+        refresh = st.form_submit_button('Get Data')
+    if refresh:
+        f={}
+        zoom=2
+        if region_sel !='All':
+            zoom=5
+            f.update({'region':region_sel})
+        if district_sel !='All':
+            zoom=8
+            f.update({'district':district_sel})
+        if port_sel !='All':
+            if port_sel =='Currently being updated':
+                f = {'id':{'$in':sites}}
+                zoom=3
+            else:
+                zoom=10
+                f.update({'name':port_sel})
+        f.update(mongo_queries.timeframe(timeframe))
+
+
+        data = mongo_queries.get_last_run_by_filter(f)
+        #catch empty data
+        if not data:
+            st.write('No data available')
         else:
-            zoom=10
-            f.update({'name':port_sel})
-    f.update(mongo_queries.timeframe(timeframe))
 
-
-    data = mongo_queries.get_last_run_by_filter(f)
-    #catch empty data
-    if not data:
-        st.write('No data available')
-    else:
-
-        df = pd.DataFrame(data)
-        
-        
-        df['local_time'] = df.apply(get_local,axis=1)
-        df['color'] = df.apply(return_color,axis=1)
-        df[['lat','long']] = df['dest'].str.split(', ',expand=True)
-        pd.to_numeric(df['lat'],errors='coerce')
-        pd.to_numeric(df['long'],errors='coerce')
-        df.lat.astype(str).astype(float)
-        df.long.astype(str).astype(float)
-        df['coordinates'] = df.apply(make_coords,axis=1)
-        mid_lat = (np.max(df['lat'].astype(float)) + np.min(df['lat'].astype(float)))/2
-        mid_long = (np.max(df['long'].astype(float)) + np.min(df['long'].astype(float)))/2
-
-        midpoint = (mid_lat,mid_long)
-        #midpoint = (np.average(df['lat'].astype(float)), np.average(df['long'].astype(float)))
-
-        cols=['name', 'wait', 'local_time']
-        df[cols]
-        layer = pdk.Layer(
-            "ScatterplotLayer",
-            df,
-            pickable=True,
-            opacity=0.8,
-            stroked=True,
-            filled=True,
-            radius_scale=6,
-            radius_min_pixels=10,
-            radius_max_pixels=100,
-            line_width_min_pixels=1,
-            get_position='coordinates',
-            get_radius=15,
-            get_fill_color='color',
-            get_line_color=[0, 0, 0],
-        )
-        view_state = pdk.ViewState(latitude=midpoint[0], longitude=midpoint[1], zoom=zoom, bearing=0, pitch=0)
-        r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{name}\nDelay: {wait} seconds"}, map_style = 'road')
-        st.write("### Map")
-        st.pydeck_chart(r)
-
-        ####
-        
-        f.update(mongo_queries.timeframe(timeframe))
-        
-        hist = pd.DataFrame(mongo_queries.get_hist_data(f)) # get the data
-        hist['utc'] = hist['utc'].dt.tz_localize('UTC')
-        hist['local_time'] = hist.apply(get_local,axis=1) # convert UTC to local time
-        
-        alt_color = {'Maximum':'max(wait):Q', 'Average':'average(wait):Q','Median':'median(wait):Q'}
-        
-        st.write('### Schedule View')
-        scatter = alt.Chart(hist).mark_rect().encode(
-            alt.Y('hours(local_time):O', title='hour of day',),
-            alt.X('day(local_time):O', title='Weekday'),        
-            alt.Tooltip(['hours(local_time):O','day(local_time):O',alt_color[metric]]),
-            color=alt_color[metric],
+            df = pd.DataFrame(data)
             
-        ).interactive()
-        st.altair_chart(scatter, use_container_width=True)
-        ####
-        st.write('### Trend ('+timeframe+')')
-        one_day = alt.Chart(hist).mark_bar().encode(
-            x='local_time:T',
-            y=alt_color[metric]
-        )
-        st.altair_chart(one_day, use_container_width=True)
+            
+            df['local_time'] = df.apply(get_local,axis=1)
+            df['color'] = df.apply(return_color,axis=1)
+            df[['lat','long']] = df['dest'].str.split(', ',expand=True)
+            pd.to_numeric(df['lat'],errors='coerce')
+            pd.to_numeric(df['long'],errors='coerce')
+            df.lat.astype(str).astype(float)
+            df.long.astype(str).astype(float)
+            df['coordinates'] = df.apply(make_coords,axis=1)
+            mid_lat = (np.max(df['lat'].astype(float)) + np.min(df['lat'].astype(float)))/2
+            mid_long = (np.max(df['long'].astype(float)) + np.min(df['long'].astype(float)))/2
 
-        ####
-        st.write('### Distribution')
-        histo = alt.Chart(hist).transform_joinaggregate(
-        total='count(*)'
-        ).transform_calculate(
-            pct='1 / datum.total'
-        ).mark_bar().encode(
-            alt.X('wait:Q', bin=True,title='Wait time (s)'),
-            alt.Y('sum(pct):Q', axis=alt.Axis(format='%'), title='Percent of count')
-        )
+            midpoint = (mid_lat,mid_long)
+            #midpoint = (np.average(df['lat'].astype(float)), np.average(df['long'].astype(float)))
 
-        # histo = alt.Chart(hist).mark_bar().encode(
-        #     alt.X('wait:Q', bin=True),
-        #     #y='count()',
-        #     y=alt.Y('count():Q',stack=None),
-        
-        # )
-        st.altair_chart(histo, use_container_width=True)
-        ###
-        f.update(mongo_queries.timeframe(timeframe))
-        st.write("### Comparison to legacy system")
-        f2 = f
-        f2['id'] = 705
-        if 'name' in f2.keys():
-            f2.pop("name")
-        ####
-        versus = pd.DataFrame(mongo_queries.prepare_legacy_compare(f2))
-        #versus['local_time'] = versus.apply(get_local,axis=1),
-        
+            cols=['name', 'wait', 'local_time']
+            df[cols]
+            layer = pdk.Layer(
+                "ScatterplotLayer",
+                df,
+                pickable=True,
+                opacity=0.8,
+                stroked=True,
+                filled=True,
+                radius_scale=6,
+                radius_min_pixels=10,
+                radius_max_pixels=100,
+                line_width_min_pixels=1,
+                get_position='coordinates',
+                get_radius=15,
+                get_fill_color='color',
+                get_line_color=[0, 0, 0],
+            )
+            view_state = pdk.ViewState(latitude=midpoint[0], longitude=midpoint[1], zoom=zoom, bearing=0, pitch=0)
+            r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{name}\nDelay: {wait} seconds"}, map_style = 'road')
+            st.write("### Map")
+            st.pydeck_chart(r)
 
-        
-        
-        versusChart = alt.Chart(versus).mark_line(point=True).encode(
-            #alt.X('local_time:T'),
-            alt.X('utc:T', title='Date'),
-            alt.Y('wait:Q'),
-            color='type',
-            shape='name'
-        ).interactive()
-        
-        st.altair_chart(versusChart, use_container_width=True)
+            ####
+            
+            f.update(mongo_queries.timeframe(timeframe))
+            
+            hist = pd.DataFrame(mongo_queries.get_hist_data(f)) # get the data
+            hist['utc'] = hist['utc'].dt.tz_localize('UTC')
+            hist['local_time'] = hist.apply(get_local,axis=1) # convert UTC to local time
+            
+            alt_color = {'Maximum':'max(wait):Q', 'Average':'average(wait):Q','Median':'median(wait):Q'}
+            
+            st.write('### Schedule View')
+            scatter = alt.Chart(hist).mark_rect().encode(
+                alt.Y('hours(local_time):O', title='hour of day',),
+                alt.X('day(local_time):O', title='Weekday'),        
+                alt.Tooltip(['hours(local_time):O','day(local_time):O',alt_color[metric]]),
+                color=alt_color[metric],
+                
+            ).interactive()
+            st.altair_chart(scatter, use_container_width=True)
+            ####
+            st.write('### Trend ('+timeframe+')')
+            one_day = alt.Chart(hist).mark_bar().encode(
+                x='local_time:T',
+                y=alt_color[metric]
+            )
+            st.altair_chart(one_day, use_container_width=True)
+
+            ####
+            st.write('### Distribution')
+            histo = alt.Chart(hist).transform_joinaggregate(
+            total='count(*)'
+            ).transform_calculate(
+                pct='1 / datum.total'
+            ).mark_bar().encode(
+                alt.X('wait:Q', bin=True,title='Wait time (s)'),
+                alt.Y('sum(pct):Q', axis=alt.Axis(format='%'), title='Percent of count')
+            )
+
+            # histo = alt.Chart(hist).mark_bar().encode(
+            #     alt.X('wait:Q', bin=True),
+            #     #y='count()',
+            #     y=alt.Y('count():Q',stack=None),
+            
+            # )
+            st.altair_chart(histo, use_container_width=True)
+            ###
+            f.update(mongo_queries.timeframe(timeframe))
+            st.write("### Comparison to legacy system")
+            f2 = f
+            f2['id'] = 705
+            pops = ['region', 'district', 'name']
+            for p in pops:
+                if p in f2.keys():
+                    f2.pop(p)
+
+            ####
+            versus = pd.DataFrame(mongo_queries.prepare_legacy_compare(f2))
+            #versus['local_time'] = versus.apply(get_local,axis=1),
+            f2 
+            versus
+
+            
+            
+            versusChart = alt.Chart(versus).mark_line(point=True).encode(
+                #alt.X('local_time:T'),
+                alt.X('utc:T', title='Date'),
+                alt.Y('wait:Q'),
+                color='type',
+                shape='name'
+            ).interactive()
+            
+            st.altair_chart(versusChart, use_container_width=True)
 
 with st.expander('User: Traveller'):
     st.write("## Travellers BWT Example")
     st.write('#### Note, each run of this is an API call to Google.')
     gmaps = mongo_queries.gmaps
     col = mongo_queries.col
-    use = st.checkbox('Use my current location')
-    if not use:
+    with st.form(key='travel'):
+        
+        
         start = st.text_input('Start location','Philadelphia, PA')
-    stop = st.text_input('Destination in Canada','Peterborough, ON')
-    tcol = st.columns(2)
-    when = tcol[0].date_input('When are you leaving?',dt.datetime.now())
-    time = tcol[1].time_input('Time?', dt.datetime.now())
-    submit = st.button('Estimate')
+        use = st.checkbox('Use my current location')
+        stop = st.text_input('Destination in Canada','Peterborough, ON')
+        tcol = st.columns(2)
+        when = tcol[0].date_input('When are you leaving?',dt.datetime.now())
+        time = tcol[1].time_input('Time?', dt.datetime.now())
+        submit = st.form_submit_button('Estimate')
     dtime = dt.datetime.combine(when,time)
     gc = gmaps.geocode(stop)
     for x in gc[0]['address_components']:

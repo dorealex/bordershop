e4df44b73fa5f06883b38939717294866753ec6c
dorealex <dorealex1@gmail.com>
2022-04-08 12:46:18 -0400

    removed traces for comparison chart

--- app.py
+++ app.py
@@ -350,8 +350,8 @@ with st.expander('User: Management'):
             ####
             versus = pd.DataFrame(mongo_queries.prepare_legacy_compare(f2))
             #versus['local_time'] = versus.apply(get_local,axis=1),
-            f2 
-            versus
+            #f2 
+            #versus
 
             
             
